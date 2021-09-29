308cc25698296b5c4995566e23ed1c911a8c73ce
arnolddominggos0 <arnolddominggos@student.telkomuniversity.ac.id>
2021-09-29 10:05:08 +0700

    HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+   public static void main(String[] args) {
+       System.out.println("Hello Arnold Sitompul!");
+	   System.out.println("NIM 6706213024");
+	   System.out.println("D3RPLA 4501");
+    
+   }
+}
